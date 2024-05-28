3eb4d897f352be37d068980a060d49482869964d
exaul <137360873+exaul@users.noreply.github.com>
2024-05-28 14:47:45 -0500

    nuevos cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import ComponentImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 const mydata = ["Exaul", "Falcones", "22", "software developer"];
 
@@ -18,14 +21,25 @@ function Header() {
     </header>
   );
 }
-function Lista() {
+
+function Lista(props) {
   return (
     <div>
-      <p>Nombre : {mydata[0]}</p>
-      <p>Apellido : {mydata[1]}</p>
-      <p>Edad : {mydata[2]}</p>
-      <p>Titulo : {mydata[3]}</p>
+      <p>Nombre: {props.nombre}</p>
+      <p>Apellido: {props.apellido}</p>
+      <p>Edad: {props.edad}</p>
+      <p>Título: {props.titulo}</p>
     </div>
+  );
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
   );
 }
 
@@ -35,7 +49,32 @@ function App() {
       <Header />
       <main>
         <h2>Time to get started!</h2>
-        <Lista />
+        {/* <Lista nombre={mydata[0]} apellido={mydata[1]} edad={mydata[2]} titulo= {mydata[3]}></Lista> */}
+        <section id="core-concepts">
+          <h2>CoreConcepts</h2>
+          <ul>
+            <CoreConcept
+              image={ComponentImg}
+              title="components"
+              description="Hola que tal como estas"
+            />
+            <CoreConcept
+              image={CORE_CONCEPTS[1].image}
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+            />
+            <CoreConcept
+              image={CORE_CONCEPTS[2].image}
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+            />
+            <CoreConcept
+              image={CORE_CONCEPTS[3].image}
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
