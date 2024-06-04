88f6f1fcd06168f206e48c75beaca89cdf8ad957
exaul <137360873+exaul@users.noreply.github.com>
2024-06-03 20:30:58 -0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,64 @@
 import React from "react";
 import ComponentImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data";
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-const mydata = ["Exaul", "Falcones", "22", "software developer"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function Lista(props) {
-  return (
-    <div>
-      <p>Nombre: {props.nombre}</p>
-      <p>Apellido: {props.apellido}</p>
-      <p>Edad: {props.edad}</p>
-      <p>Título: {props.titulo}</p>
-    </div>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import Header from "./Components/Header/Header";
+import CoreConcept from "./Components/CoreConcept/CoreConcept";
+import TabButton from "./Components/TabButton/TabButton";
 
 function App() {
+  function handleClick(selectedButton) {
+    console.log(selectedButton);
+  }
+
   return (
     <div>
       <Header />
       <main>
         <h2>Time to get started!</h2>
-        {/* <Lista nombre={mydata[0]} apellido={mydata[1]} edad={mydata[2]} titulo= {mydata[3]}></Lista> */}
         <section id="core-concepts">
           <h2>CoreConcepts</h2>
           <ul>
-            <CoreConcept
-              image={ComponentImg}
-              title="components"
-              description="Hola que tal como estas"
-            />
-            <CoreConcept
-              image={CORE_CONCEPTS[1].image}
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-            />
-            <CoreConcept
-              image={CORE_CONCEPTS[2].image}
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-            />
-            <CoreConcept
-              image={CORE_CONCEPTS[3].image}
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-            />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2></h2>
+          <section id="examples">
+            <TabButton
+              handleClick={() => {
+                handleClick("Components");
+              }}
+            >
+              Components
+            </TabButton>
+            <br />
+            <TabButton
+              handleClick={() => {
+                handleClick("JSX");
+              }}
+            >
+              JSX
+            </TabButton>
+            <br />
+            <TabButton
+              handleClick={() => {
+                handleClick("Props");
+              }}
+            >
+              Props
+            </TabButton>
+            <br />
+            <TabButton
+              handleClick={() => {
+                handleClick("state");
+              }}
+            >
+              state
+            </TabButton>
+          </section>
         </section>
       </main>
     </div>
