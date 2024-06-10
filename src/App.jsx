8074ec133b2be9495adb84b8612b47fd95801762
exaul <137360873+exaul@users.noreply.github.com>
2024-06-10 14:42:37 -0500

    added props and destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-import React from "react";
-import { CORE_CONCEPTS } from "./data";
-import Header from "./Components/Header/Header";
-import CoreConcept from "./Components/CoreConcept/CoreConcept";
-import TabButton from "./Components/TabButton/TabButton";
-import { useState } from "react";
-import { EXAMPLES } from "./data";
-
-function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleClick(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
-
-=======
-import Header from "./Components/Header.jsx";
+import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import { CORE_CONCEPTS } from "./data";
 
 function App() {
->>>>>>> b987b0121f4fb321b4e4b10d05b7d5105d19d9a4
   return (
     <div>
       <Header />
@@ -32,63 +14,6 @@ function App() {
           <h2>Core Concepts</h2>
 
           <ul>
-<<<<<<< HEAD
-            {CORE_CONCEPTS.map((concepItem) => (
-              <CoreConcept {...concepItem} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <section id="examples">
-            <menu>
-              <TabButton
-                handleClick={() => {
-                  handleClick("components");
-                }}
-              >
-                Components
-              </TabButton>
-              <br />
-              <TabButton
-                handleClick={() => {
-                  handleClick("jsx");
-                }}
-              >
-                JSX
-              </TabButton>
-              <br />
-              <TabButton
-                handleClick={() => {
-                  handleClick("props");
-                }}
-              >
-                Props
-              </TabButton>
-              <br />
-              <TabButton
-                handleClick={() => {
-                  handleClick("state");
-                }}
-              >
-                state
-              </TabButton>
-            </menu>
-            {selectedTopic === undefined ? (
-              <p> Please select a topic.</p>
-            ) : (
-              <div>
-                <div id="tab-content">
-                  <h3>{EXAMPLES[selectedTopic].title}</h3>
-                  <p>{EXAMPLES[selectedTopic].description}</p>
-                </div>
-                <pre>
-                  <code>{EXAMPLES[selectedTopic].code}</code>
-                </pre>
-              </div>
-            )}
-          </section>
-        </section>
-=======
             <CoreConcept
               image={CORE_CONCEPTS[0].image}
               title={CORE_CONCEPTS[0].title}
@@ -106,7 +31,6 @@ function App() {
             />
           </ul>
         </section>
->>>>>>> b987b0121f4fb321b4e4b10d05b7d5105d19d9a4
       </main>
     </div>
   );
